7d9774d607355f16e2490485b5d958475ef45250
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 15:40:35 +0900

    Implement Mover

--- Assets/Scripts/Characters/Commons/CharacterMover.cs
+++ Assets/Scripts/Characters/Commons/CharacterMover.cs
@@ -7,11 +7,40 @@ namespace Characters.Commons
     [RequireComponent(typeof(Rigidbody))]
     public class CharacterMover : MonoBehaviour
     {
+        [SerializeField]
+        Rigidbody _rigidbody;
+        [SerializeField]
+        float speed;
+
         [Inject]
         ICharacterCore characterCore;
 
         void Start()
         {
+            characterCore.OnMoveAsObservable()
+                .ObserveOn(Scheduler.MainThreadFixedUpdate)
+                .Select(v => v.normalized * speed * Time.deltaTime)
+                .Subscribe(Move)
+                .AddTo(this);
+
+            characterCore.OnRotateAsObservable()
+                .ObserveOn(Scheduler.MainThreadFixedUpdate)
+                .Select(v => Quaternion.LookRotation(new Vector3(v.x, 0, v.y), Vector3.up))
+                .Subscribe(Rotate)
+                .AddTo(this);
+        }
+
+        void Move(Vector2 delta)
+        {
+            var position = _rigidbody.position;
+            position.x += delta.x;
+            position.z += delta.y;
+            _rigidbody.position = position;
+        }
+
+        void Rotate(Quaternion rotation)
+        {
+            _rigidbody.MoveRotation(rotation);
         }
     }
 }
