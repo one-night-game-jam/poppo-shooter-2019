5c09b762eae09975633f18bd8b538cba1605f2fa
Shoma SATO <noir.neo.04@gmail.com>
2019-01-13 02:42:18 +0900

    Add tutorial button

---
+++ Assets/Scripts/UI/LinkButton.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class LinkButton : MonoBehaviour
+    {
+        [SerializeField]
+        Button button;
+
+        [SerializeField]
+        string url;
+
+        void Start()
+        {
+            button.OnClickAsObservable()
+                .Subscribe(_ => Application.OpenURL(url))
+                .AddTo(this);
+        }
+    }
+}
