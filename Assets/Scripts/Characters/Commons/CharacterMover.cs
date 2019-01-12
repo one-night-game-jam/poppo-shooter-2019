a29126ab3aea99edbd1259f3ee325451819d9f37
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 13:23:22 +0900

    Add mover skeleton

---
+++ Assets/Scripts/Characters/Commons/CharacterMover.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Characters.Commons
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterMover : MonoBehaviour
+    {
+        [Inject]
+        ICharacterCore characterCore;
+
+        void Start()
+        {
+        }
+    }
+}
