ea9a6b459cf85b5fdd963466e21894fab60fa2db
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 13:10:03 +0900

    Add player skelton

---
+++ Assets/Scripts/Characters/ICharacterCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Vector2 = UnityEngine.Vector2;
+
+namespace Characters
+{
+    interface ICharacterCore
+    {
+        IObservable<Vector2> OnMoveAsObservable();
+        IObservable<Vector2> OnFireAsObservable();
+        IObservable<Vector2> OnBoostAsObservable();
+    }
+}
