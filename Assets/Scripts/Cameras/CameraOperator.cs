5d473951a1ae65b051e6ca5f88ec5161919a3a56
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 16:07:40 +0900

    Add follow camera

---
+++ Assets/Scripts/Cameras/CameraOperator.cs
@@ -0,0 +1,30 @@
+﻿using Characters.Players;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Cameras
+{
+    public class CameraOperator : MonoBehaviour
+    {
+        [Inject]
+        PlayerCore player;
+
+        void Start()
+        {
+            var y = transform.position.y;
+            var diff = transform.position - player.transform.position;
+            player.transform.ObserveEveryValueChanged(t => t.position)
+                .TakeUntilDestroy(player.transform)
+                .Select(p => p + diff)
+                .Select(p =>
+                {
+                    p.y = y;
+                    return p;
+                })
+                .ObserveOn(Scheduler.MainThreadEndOfFrame)
+                .Subscribe(p => transform.position = p)
+                .AddTo(this);
+        }
+    }
+}
