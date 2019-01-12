7351cccabd10c35a62eead8f465bd90dd8b4d63d
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 15:50:52 +0900

    Add commons skeleton

---
+++ Assets/Scripts/Characters/Commons/CharacterBooster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Characters.Commons
+{
+    public class CharacterBooster : MonoBehaviour
+    {
+        [SerializeField]
+        double throttleMillis;
+
+        [Inject]
+        ICharacterCore characterCore;
+
+        void Start()
+        {
+            characterCore.OnBoostAsObservable()
+                .ThrottleFirst(TimeSpan.FromMilliseconds(throttleMillis))
+                .Subscribe(Boost)
+                .AddTo(this);
+        }
+
+        void Boost(Vector2 direction)
+        {
+            // TODO: Implement
+            Debug.Log("Boost!");
+        }
+    }
+}
