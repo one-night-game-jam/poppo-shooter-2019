4ebadbff1c6e3037f7cbbed50be0ced0e6aba565
Shoma SATO <noir.neo.04@gmail.com>
2019-01-12 18:11:08 +0900

    Add Enemy

---
+++ Assets/Scripts/Characters/Enemies/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Characters.Enemies
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        readonly ReactiveProperty<Vector2> moveDirection = new ReactiveProperty<Vector2>();
+        readonly ReactiveProperty<Vector2> targetDirection = new ReactiveProperty<Vector2>();
+        readonly ReactiveProperty<bool> fire = new ReactiveProperty<bool>();
+
+        public IReadOnlyReactiveProperty<Vector2> MoveDirection => moveDirection;
+        public IReadOnlyReactiveProperty<Vector2> TargetDirection => targetDirection;
+        public IReadOnlyReactiveProperty<bool> Fire => fire;
+    }
+}
